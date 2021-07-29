167ea21d514c3fe5ca1478211261a6c420f12662
Dan Sheldon <sheldon@cs.umass.edu>
2021-07-29 18:42:57 -0400

    script to update locations files from web if needed

---
+++ scripts/resources/update_locations.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wget https://raw.githubusercontent.com/reichlab/covid19-forecast-hub/master/data-locations/locations.csv -O locations.csv
+wget https://raw.githubusercontent.com/epiforecasts/covid19-forecast-hub-europe/main/data-locations/locations_eu.csv -O locations_eu.csv
